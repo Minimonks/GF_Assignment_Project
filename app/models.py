a6930697b769b21560b221e3a0b92b8ea4510a86
Minimonks <georgefillery@live.co.uk>
2023-03-19 11:30:34 +0000

    Hopefully correct Models so that DB marks as not null

--- app/models.py
+++ app/models.py
@@ -3,8 +3,8 @@ from . import db
 #Defining "Models" (Tables)
 class Role(db.Model): 
     __tablename__ = 'Role' 
-    RoleID = db.Column(db.Integer, primary_key=True) 
-    RoleName = db.Column(db.String(64), unique=True) 
+    RoleID = db.Column(db.Integer, primary_key=True, nullable=False) 
+    RoleName = db.Column(db.String(64), unique=True, nullable=False) 
 
     Users = db.relationship('User', backref='role', lazy='dynamic')
     
@@ -13,11 +13,11 @@ class Role(db.Model):
         
 class User(db.Model): 
     __tablename__ = 'User' 
-    UserID = db.Column(db.Integer, primary_key=True) 
-    Username = db.Column(db.String(125), unique=True, index=True) 
-    Password = db.Column(db.String(125)) 
-    Email = db.Column(db.String(255), unique=True, index=True) 
-    RoleID = db.Column(db.Integer, db.ForeignKey('Role.RoleID'))
+    UserID = db.Column(db.Integer, primary_key=True, nullable=False) 
+    Username = db.Column(db.String(125), unique=True, index=True, nullable=False) 
+    Password = db.Column(db.String(125), nullable=False) 
+    Email = db.Column(db.String(255), unique=True, index=True, nullable=False) 
+    RoleID = db.Column(db.Integer, db.ForeignKey('Role.RoleID'), nullable=False)
     
     User_Requests = db.relationship('UserRequest', backref='user', lazy=True)
     
@@ -27,11 +27,11 @@ class User(db.Model):
 class SoftwareRequest(db.Model):
     __tablename__ = 'SoftwareRequest' 
     RequestID = db.Column(db.Integer, primary_key=True)
-    RequestTitle = db.Column(db.String(64)) #Strayed a little off the model in a few areas...
-    RequestDetails = db.Column(db.String(255))
+    RequestTitle = db.Column(db.String(64), nullable=False) #Strayed a little off the model in a few areas...
+    RequestDetails = db.Column(db.String(255), nullable=False)
     RequestImpact = db.Column(db.String(255), nullable=True)
     RequestDeadline = db.Column(db.DateTime, nullable=True)
-    RequestImportance = db.Column(db.Integer)
+    RequestImportance = db.Column(db.Integer, nullable=False)
     RequestAccepted = db.Column(db.Boolean, nullable=True)
 
     user_request = db.relationship('UserRequest', backref='software_request', uselist=False)
@@ -41,5 +41,5 @@ class SoftwareRequest(db.Model):
     
 class UserRequest(db.Model):
     __tablename__ = 'UserRequest' 
-    UserID = db.Column(db.Integer, db.ForeignKey('User.UserID') ,primary_key=True)
-    RequestId = db.Column(db.Integer, db.ForeignKey('SoftwareRequest.RequestID'),primary_key=True)
+    UserID = db.Column(db.Integer, db.ForeignKey('User.UserID') ,primary_key=True, nullable=False)
+    RequestId = db.Column(db.Integer, db.ForeignKey('SoftwareRequest.RequestID'),primary_key=True, nullable=False)
