0686aa181a1596b690c5c3aa82d0a4ccf2a9416b
Minimonks <georgefillery@live.co.uk>
2023-03-19 11:55:25 +0000

    Tidy before request acc

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,10 +5,7 @@ from.forms import LoginForm
 
 main = Blueprint('main',__name__)
 
-@main.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
+@main.route('/', methods=['GET', 'POST'])
 @main.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -16,6 +13,10 @@ def login():
         # handle login logic
         return redirect(url_for('main.index'))
     return render_template('login.html', form=form)
+
+@main.route("/home/")
+def home():
+    return render_template('index.html')
 
 @main.route("/about/")
 def about():
