8d0a5d9a27826e68a74c71b92e84a3f34ddbc0c8
Minimonks <georgefillery@live.co.uk>
2024-02-19 14:04:41 +0000

    Inital SQLiteTransition... DB should be created with roles inserted

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ class Config(object):
 #Development configuration, please adjust SQLALCHEMY_DATABASE_URI to your DB Details. Link below is an example connection to an SSMS DB with Windows authentication.
 class Development(Config):
     DEBUG=True
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://:@(LocalDB)\GeorgesLocalDB/GFAssignmentDB?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes'
+    #SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://:@(LocalDB)\GeorgesLocalDB/GFAssignmentDB?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///GFSEDODB.db'
 
 #Testing configuration, please adjust SQLALCHEMY_DATABASE_URI to your DB Details. Link below is an example connection to an SSMS DB with Windows authentication. DO NOT SET THE SAME AS DEV DB AS UNIT TESTS WILL DROP DATA.
 class Testing(Config):
