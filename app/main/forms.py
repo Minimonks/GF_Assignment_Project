b965624b83ca3926089ef560345d8df5683621c9
Minimonks <georgefillery@live.co.uk>
2024-03-03 12:56:08 +0000

    Added password rules

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,7 +2,8 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, DateField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, EqualTo, Length, Optional, NumberRange
+from wtforms.validators import DataRequired, EqualTo, Length, Optional, NumberRange, regexp
+import re
 
 
 class LoginForm(FlaskForm):
@@ -13,7 +14,7 @@ class LoginForm(FlaskForm):
 class RequestAccountForm(FlaskForm):
     username = StringField('Username:', validators=[DataRequired()])
     email = StringField('Email:', validators=[DataRequired()]) #, Email()
-    password = PasswordField('Password:',  validators=[DataRequired(), EqualTo('confirmPassword', message='Passwords must match')])
+    password = PasswordField('Password:',  validators=[DataRequired(), EqualTo('confirmPassword', message='Passwords must match'), Length(min=8, message="Password must be at least 8 characters long."), regexp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$', message="Password must contain at least one uppercase letter, one lowercase letter, one number and one special character.")])
     confirmPassword = PasswordField('Confirm Password:', validators=[DataRequired()])
     role = BooleanField('Admin Account') #May be more roles in future ofc... but now... only admin/user
     submit = SubmitField('Request Account')
